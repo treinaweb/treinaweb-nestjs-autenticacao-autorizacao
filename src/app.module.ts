d03a1fff057f4676346ac8e11fd7fb99acc88a71
Wesley Gado <wesleygado@MacBook-Air-de-Wesley.local>
2023-07-19 16:30:05 -0300

    2 - Criando banco de dados e configurando TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'treinaweb',
+      database: 'app-aa',
+      entities: [join(__dirname, '**/*entity.{ts,js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
